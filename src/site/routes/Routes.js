ac65c2b655e62b764bcbf5984b5a9d54a2bfc11d
Amor Ali <alibenamor30@gmail.com>
2020-11-25 21:08:39 +0100

    Inbox admin route

--- src/site/routes/Routes.js
+++ src/site/routes/Routes.js
@@ -6,7 +6,7 @@ import NotFound from "../pages/NotFound";
 import Home from "../pages/Home";
 import Store from "../pages/Store";
 import Dashboard from "../pages/admin/Dashboard";
-
+import Inbox from "../pages/admin/Inbox";
 export default function Routes() {
   return (
     <Switch>
@@ -22,6 +22,7 @@ export default function Routes() {
       <Route exact path="/admin">
         <Dashboard />
       </Route>
+      <Route exact path="/admin/inbox"></Route>
       <Route path="/404">
         <NotFound />
       </Route>
