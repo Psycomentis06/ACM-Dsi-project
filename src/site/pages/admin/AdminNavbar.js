0948c66c94e22373349fffd48a52f648c45f9398
Amor Ali <alibenamor30@gmail.com>
2020-11-25 23:46:42 +0100

    Routes navigation + dropdown added

--- src/site/pages/admin/AdminNavbar.js
+++ src/site/pages/admin/AdminNavbar.js
@@ -1,9 +1,30 @@
 import React, { useState } from "react";
-import { Navbar, Nav, NavItem, NavLink, NavbarToggler } from "reactstrap";
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarToggler,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+} from "reactstrap";
+import { Link, useHistory } from "react-router-dom";
 import "./dashboard.scss";
 import AdminAvatar from "../../../assets/images/avatar.svg";
 export default function AdminNavbar() {
   const [openSlider, setOpenSlider] = useState(false);
+  const [navDropdown, setNavDropdown] = useState(false);
+  const navDropdownHandler = () => {
+    setNavDropdown(!navDropdown);
+  };
+  let history = useHistory();
+
+  const logout = () => {
+    localStorage.removeItem("token"); // lose token
+    history.push("/");
+  };
   return (
     <>
       <Navbar expand="md" className="gradient-full" color="light" light>
@@ -16,21 +37,49 @@ export default function AdminNavbar() {
         <div className={"navbar-slider " + (openSlider ? "show" : "")}>
           <Nav>
             <NavItem>
-              <NavLink href="#">Dashboard</NavLink>
+              <Link to="/admin/" className="nav-link">
+                Dashboard
+              </Link>
             </NavItem>
             <NavItem>
-              <NavLink href="#">Users</NavLink>
+              <Link to="/admin/users" className="nav-link">
+                Users
+              </Link>
             </NavItem>
             <NavItem>
-              <NavLink className="admin-logo" href="#">
+              <NavLink className="admin-logo">
                 <img src={AdminAvatar} alt="Admin avatar" />
               </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="#">Inbox</NavLink>
+              <Link to="/admin/inbox" className="nav-link">
+                Inbox
+              </Link>
             </NavItem>
             <NavItem>
-              <NavLink href="#">Orders</NavLink>
+              <Dropdown
+                isOpen={navDropdown}
+                toggle={navDropdownHandler}
+                inNavbar={true}
+              >
+                <DropdownToggle
+                  style={{
+                    backgroundColor: "transparent",
+                    borderColor: "transparent",
+                  }}
+                >
+                  <i className="fas fa-sort-down fa-2x"></i>
+                </DropdownToggle>
+                <DropdownMenu style={{ left: "auto", right: 0 }}>
+                  <DropdownItem>
+                    <Link to="/admin/orders" style={{ textDecoration: "none" }}>
+                      Orders
+                    </Link>
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem onClick={logout}>Logout</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
             </NavItem>
           </Nav>
         </div>
