51390efa534a2e06f75ce8e0d51c091019a45f91
Amor Ali <alibenamor30@gmail.com>
2020-11-25 20:38:06 +0100

    navbar removed

--- src/site/pages/admin/Admin.js
+++ src/site/pages/admin/Admin.js
@@ -1,20 +1,12 @@
 import React from "react";
 
 import AdminNavbar from "./AdminNavbar";
-//import UserChatRoomLink from "../../components/UserChatRoomLink";
-import Chat from "../../components/Chat";
+import Inbox from "./Inbox";
 export default function Admin() {
   return (
     <>
       <AdminNavbar />
-      <Chat />
-      {/*
-      <UserChatRoomLink />
-      <UserChatRoomLink />
-      <UserChatRoomLink />
-      <UserChatRoomLink />
-      <UserChatRoomLink />
-        */}
+      <Inbox />
     </>
   );
 }
