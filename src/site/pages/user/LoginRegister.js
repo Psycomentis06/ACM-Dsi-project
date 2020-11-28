4ca73be1956332f1e7ac206d5e565509f3bdca18
Amor Ali <alibenamor30@gmail.com>
2020-11-28 23:04:27 +0100

    Animation 100% works

--- src/site/pages/user/LoginRegister.js
+++ src/site/pages/user/LoginRegister.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Login from "../../components/Login";
 import Signup from "../../components/Signup";
 import "./LoginRegister.scss";
@@ -14,43 +15,61 @@ export default function LoginRegister() {
       <div className="login-register-container">
         <div className="login-register">
           <div className={registerView ? "login-row reverse" : "login-row"}>
-            <div className="col1">{registerView ? <Signup /> : <Login />}</div>
-            <div className="col2 shadow-3 bg-gradient-purple-90">
-              <div className="content">
-                <Link
-                  to="/"
-                  className="btn my-3 bg-gradient-purple-45 rounded-circle shadow-3"
+            <div className="col1">
+              <div style={{ height: "100%" }}>
+                <CSSTransition
+                  in={registerView}
+                  timeout={800}
+                  classNames="col1-animation"
                 >
-                  <i class="fas fa-arrow-left"></i>
-                </Link>
-                <h3 className={registerView ? "text-right" : "text-left"}>
-                  {registerView ? "Login" : "Register"}
-                </h3>
-                <p
-                  className={registerView ? "h6 text-right" : "h6 text-justify"}
-                >
-                  {registerView ? loginText : registerText}
-                </p>
-                <div>
-                  {registerView ? (
-                    <button
-                      style={{ float: "right" }}
-                      className="text-primary my-3 btn bg-gradient p-2"
-                      onClick={() => setRegisterView(false)}
-                    >
-                      Login
-                    </button>
-                  ) : (
-                    <button
-                      className="text-primary my-3 btn bg-gradient p-2"
-                      onClick={() => setRegisterView(true)}
-                    >
-                      Register
-                    </button>
-                  )}
-                </div>
+                  {registerView ? <Signup /> : <Login />}
+                </CSSTransition>
               </div>
             </div>
+            <CSSTransition
+              in={registerView}
+              timeout={800}
+              classNames="col2-animation"
+            >
+              <div className="col2 shadow-3 bg-gradient-purple-90">
+                <div className="content">
+                  <Link
+                    to="/"
+                    className="btn my-3 bg-gradient-purple-45 rounded-circle shadow-3"
+                  >
+                    <i className="fas fa-arrow-left"></i>
+                  </Link>
+                  <h3 className={registerView ? "text-right" : "text-left"}>
+                    {registerView ? "Login" : "Register"}
+                  </h3>
+                  <p
+                    className={
+                      registerView ? "h6 text-right" : "h6 text-justify"
+                    }
+                  >
+                    {registerView ? loginText : registerText}
+                  </p>
+                  <div>
+                    {registerView ? (
+                      <button
+                        style={{ float: "right" }}
+                        className="text-primary my-3 btn bg-gradient font-weight-bolder p-2"
+                        onClick={() => setRegisterView(false)}
+                      >
+                        Login
+                      </button>
+                    ) : (
+                      <button
+                        className="text-primary my-3 btn bg-gradient font-weight-bolder p-2"
+                        onClick={() => setRegisterView(true)}
+                      >
+                        Register
+                      </button>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </CSSTransition>
           </div>
         </div>
       </div>
