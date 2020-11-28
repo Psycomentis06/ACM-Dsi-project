23cc19a754c98d8f6947b3803016933230924e84
Amor Ali <alibenamor30@gmail.com>
2020-11-28 14:53:47 +0100

    UI ready

--- src/site/components/Login.js
+++ src/site/components/Login.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Button, Col, Input, Row } from "reactstrap";
+import React, { useState } from "react";
+import { Form, Col, Input, Row, FormFeedback } from "reactstrap";
 import "./Login.scss";
 export default function Login() {
   return (
@@ -8,36 +8,40 @@ export default function Login() {
         <i className="fas fa-user-tie fa-5x"></i>
       </div>
       <div className="content">
-        <h2 className="text-center">Login</h2>
-        <Row className="mt-4">
-          <Col>
-            <div className="input-float">
-              <label htmlFor="__email" className="label bg-white">
-                Email
-              </label>
-              <Input type="email" placeholder="User Email" id="__email" />
-            </div>
-          </Col>
-        </Row>
-        <Row className="mt-4">
-          <Col>
-            <div className="input-float">
-              <label htmlFor="__password" className="label bg-white">
-                Password
-              </label>
-              <Input
-                type="password"
-                placeholder="User password"
-                id="__password"
-              />
-            </div>
-          </Col>
-        </Row>
-        <Row className="mt-4">
-          <button className="btn bg-gradient-purple rounded-pill mx-auto font-weight-bolder">
-            Login
-          </button>
-        </Row>
+        <Form>
+          <Row className="mt-4">
+            <Col>
+              <div className="input-float">
+                <label htmlFor="__email" className="label bg-white">
+                  Email
+                </label>
+                <Input type="email" placeholder="User Email" id="__email" />
+              </div>
+            </Col>
+          </Row>
+          <Row className="mt-4">
+            <Col>
+              <div className="input-float">
+                <label htmlFor="__password" className="label bg-white">
+                  Password
+                </label>
+                <Input
+                  type="password"
+                  placeholder="User password"
+                  id="__password"
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row className="mt-4">
+            <button
+              type="submit"
+              className="btn bg-gradient-purple shadow-2 w-25 rounded-pill mx-auto font-weight-bolder"
+            >
+              Login
+            </button>
+          </Row>
+        </Form>
       </div>
     </div>
   );
