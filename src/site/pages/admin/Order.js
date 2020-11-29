9e82b2e25b729e093cc623f05bc21713f7c0fa83
Amor Ali <alibenamor30@gmail.com>
2020-11-29 16:30:53 +0100

    Routes and nested routes set

--- src/site/pages/admin/Order.js
+++ src/site/pages/admin/Order.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-export default function Order() {
-  const { id } = useParams();
+export default function Order(props) {
+  let { userId } = useParams();
   return (
     <div>
       Order Component
       <br />
-      orders for user id: {id}
+      orders for user id: {props.userId || userId || "not found"}
     </div>
   );
 }
--- src/site/pages/admin/Orders.js
+++ src/site/pages/admin/Orders.js
@@ -1,30 +1,42 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
+import {
+  Link,
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useRouteMatch,
+} from "react-router-dom";
+import Order from "./Order";
+import UserListItem from "../../components/UserListItem";
 export default function Orders() {
+  let { path, url } = useRouteMatch();
   return (
-    <Container>
-      <Row>
-        <Col sm="4">
-          <h3>Users</h3>
-          <hr />
-          <ul>
-            <li>user1</li>
-            <li>user2</li>
-            <li>user3</li>
-            <li>user4</li>
-          </ul>
-        </Col>
-        <Col>
-          <h3>Orders</h3>
-          <hr />
-          <ul>
-            <li>o1</li>
-            <li>o2</li>
-            <li>o3</li>
-            <li>o4</li>
-          </ul>
-        </Col>
-      </Row>
-    </Container>
+    <Router>
+      <Container className="mt-3">
+        <Row>
+          <Col sm="4">
+            <h3>Users</h3>
+            <hr />
+            <Link to={url + "/user1"}>User1</Link>
+            <Link to={url + "/user2"}>User2</Link>
+            <Link to={url + "/user3"}>User3</Link>
+            <Link to={url + "/user4"}>User4</Link>
+          </Col>
+          <Col>
+            <h3>Orders</h3>
+            <hr />
+            <Switch>
+              <Route exact path={path + "/:userId"}>
+                <Order />
+              </Route>
+              <Route path={path}>
+                <Order userId="user1" /> {/* Display first user orders */}
+              </Route>
+            </Switch>
+          </Col>
+        </Row>
+      </Container>
+    </Router>
   );
 }
