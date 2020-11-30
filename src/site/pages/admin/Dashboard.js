85437f94e3ea5506bfd9e9a42907beca1ed84f53
Amor Ali <alibenamor30@gmail.com>
2020-11-30 22:56:24 +0100

    Demo charts

--- src/site/pages/admin/Dashboard.js
+++ src/site/pages/admin/Dashboard.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Container, Row, Col } from "reactstrap";
+import { Line } from "react-chartjs-2";
 import StatCard from "../../components/StatCard";
 export default function Dashboard() {
   return (
@@ -81,6 +82,84 @@ export default function Dashboard() {
 
           <h1 className="display-4 mt-4">Charts</h1>
           <hr className="my-4" />
+          <Row>
+            <Col lg="6">
+              <Line
+                data={{
+                  labels: [
+                    "January",
+                    "February",
+                    "March",
+                    "April",
+                    "May",
+                    "June",
+                    "July",
+                  ],
+                  datasets: [
+                    {
+                      label: "Monthly Users login",
+                      fill: false,
+                      lineTension: 0.1,
+                      backgroundColor: "rgba(75,192,192,0.4)",
+                      borderColor: "rgba(75,192,192,1)",
+                      borderCapStyle: "butt",
+                      borderDash: [],
+                      borderDashOffset: 0.0,
+                      borderJoinStyle: "miter",
+                      pointBorderColor: "rgba(75,192,192,1)",
+                      pointBackgroundColor: "#fff",
+                      pointBorderWidth: 1,
+                      pointHoverRadius: 5,
+                      pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                      pointHoverBorderColor: "rgba(220,220,220,1)",
+                      pointHoverBorderWidth: 2,
+                      pointRadius: 1,
+                      pointHitRadius: 10,
+                      data: [65, 59, 80, 81, 56, 55, 40],
+                    },
+                  ],
+                }}
+              />
+            </Col>
+            <Col lg="6">
+              <Line
+                data={{
+                  labels: [
+                    "January",
+                    "February",
+                    "March",
+                    "April",
+                    "May",
+                    "June",
+                    "July",
+                  ],
+                  datasets: [
+                    {
+                      label: "Monthly ordered products",
+                      fill: false,
+                      lineTension: 0.1,
+                      backgroundColor: "rgba(75,192,192,0.4)",
+                      borderColor: "rgba(75,192,192,1)",
+                      borderCapStyle: "butt",
+                      borderDash: [],
+                      borderDashOffset: 0.0,
+                      borderJoinStyle: "miter",
+                      pointBorderColor: "rgba(75,100,100,1)",
+                      pointBackgroundColor: "#fff",
+                      pointBorderWidth: 1,
+                      pointHoverRadius: 5,
+                      pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                      pointHoverBorderColor: "rgba(220,220,220,1)",
+                      pointHoverBorderWidth: 2,
+                      pointRadius: 1,
+                      pointHitRadius: 10,
+                      data: [140, 14, 54, 142, 48, 88, 67],
+                    },
+                  ],
+                }}
+              />
+            </Col>
+          </Row>
         </Container>
       </Container>
     </>
