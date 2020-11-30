02b3ade0502ede69a66c86b4a3fe88235cc05cf9
Amor Ali <alibenamor30@gmail.com>
2020-11-30 22:06:23 +0100

    animate routes

--- src/site/routes/Routes.js
+++ src/site/routes/Routes.js
@@ -1,6 +1,10 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-
+import { Switch, Route, Redirect, useLocation } from "react-router-dom";
+import {
+  CSSTransition,
+  TransitionGroup,
+  SwitchTransition,
+} from "react-transition-group";
 // import pages
 import NotFound from "../pages/NotFound";
 import Home from "../pages/Home";
@@ -16,51 +20,62 @@ import Products from "../pages/admin/Products";
 import Product from "../pages/admin/Product";
 import Profile from "../pages/admin/Profile";
 export default function Routes() {
+  let location = useLocation();
   return (
-    <Switch>
-      {/* User Routes*/}
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/store">
-        <Store />
-      </Route>
-      <Route exact path="/login">
-        <LoginRegister />
-      </Route>
+    <SwitchTransition mode="out-in">
+      <CSSTransition
+        classNames={
+          location.pathname.split("/").length > 2 ? "sliderRight" : "sliderLeft"
+        }
+        timeout={600}
+        key={location.key}
+      >
+        <Switch location={location}>
+          {/* User Routes*/}
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/store">
+            <Store />
+          </Route>
+          <Route exact path="/login">
+            <LoginRegister />
+          </Route>
 
-      {/* Admin Routes */}
-      <Route exact path="/admin">
-        <Dashboard />
-      </Route>
-      <Route exact path="/admin/inbox">
-        <Inbox />
-      </Route>
-      <Route exact path="/admin/inbox/:id">
-        <Chat />
-      </Route>
-      <Route exact path="/admin/users">
-        <Users />
-      </Route>
-      <Route exact path="/admin/users/:userId">
-        <User />
-      </Route>
-      <Route path="/admin/orders">
-        <Orders />
-      </Route>
-      <Route exact path="/admin/products">
-        <Products />
-      </Route>
-      <Route exact path="/admin/products/:productId">
-        <Product />
-      </Route>
-      <Route path="/admin/profile">
-        <Profile />
-      </Route>
-      <Route path="/404">
-        <NotFound />
-      </Route>
-      <Redirect to="/404" />
-    </Switch>
+          {/* Admin Routes */}
+          <Route exact path="/admin">
+            <Dashboard />
+          </Route>
+          <Route exact path="/admin/inbox">
+            <Inbox />
+          </Route>
+          <Route exact path="/admin/inbox/:id">
+            <Chat />
+          </Route>
+          <Route exact path="/admin/users">
+            <Users />
+          </Route>
+          <Route exact path="/admin/users/:userId">
+            <User />
+          </Route>
+          <Route path="/admin/orders">
+            <Orders />
+          </Route>
+          <Route exact path="/admin/products">
+            <Products />
+          </Route>
+          <Route exact path="/admin/products/:productId">
+            <Product />
+          </Route>
+          <Route path="/admin/profile">
+            <Profile />
+          </Route>
+          <Route path="/404">
+            <NotFound />
+          </Route>
+          <Redirect to="/404" />
+        </Switch>
+      </CSSTransition>
+    </SwitchTransition>
   );
 }
