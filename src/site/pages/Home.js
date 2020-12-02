d9459adec4d175f4d633743d46335d1d2f82f416
Amor Ali <alibenamor30@gmail.com>
2020-12-02 22:05:45 +0100

    text SWAL2

--- src/site/pages/Home.js
+++ src/site/pages/Home.js
@@ -1,9 +1,18 @@
-import React from 'react';
-
+import React from "react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+const alert = withReactContent(Swal);
 function Home() {
-    return(
-        <h1>Home</h1>
-    );
+  return (
+    <div>
+      <h1>Home</h1>
+      <button
+        onClick={() => alert.fire("Hello there", "simple text", "success")}
+      >
+        Open
+      </button>
+    </div>
+  );
 }
 
 export default Home;
