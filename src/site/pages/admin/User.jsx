618f5f98c35ea394167577a579136e94e30c3c23
Amor Ali <alibenamor30@gmail.com>
2020-12-08 15:34:54 +0100

    .js => .jsx

--- src/site/pages/admin/User.jsx
+++ src/site/pages/admin/User.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import { useParams } from "react-router-dom";
 export default function User() {
   const { userId } = useParams();
-  return <h1>User works with ID: {userId}</h1>;
+  return <div>hello</div>;
 }
