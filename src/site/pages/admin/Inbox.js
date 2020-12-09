d410a3e7103376cbaf18f6ef7da247d6260e0e93
Amor Ali <alibenamor30@gmail.com>
2020-12-09 15:44:37 +0100

    infinite scroll + get users 90%

--- src/site/pages/admin/Inbox.js
+++ src/site/pages/admin/Inbox.js
@@ -1,12 +1,131 @@
-import React, { useState, use, useEffect } from "react";
-import { Row, Col, Container, Input, Alert } from "reactstrap";
-import Axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Row, Col, Container, Input, Alert, Spinner } from "reactstrap";
+import firebase from "../../../firebase.config";
+import useInfiniteScroll from "react-infinite-scroll-hook";
+//import Axios from "axios";
 import UserChatRoom from "../../components/UserChatRoomLink";
-import { Redirect } from "react-router-dom";
 import LoadingPage from "../../components/LoadingPage";
+//import { Redirect } from "react-router-dom";
+//import LoadingPage from "../../components/LoadingPage";
 export default function Inbox() {
   const [loadingPage, setLoadingPage] = useState(true);
-  const [users, setUsers] = useState([]);
+  const [rooms, setRooms] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [hasNextPage, setHasNextPage] = useState(true);
+
+  const getRooms = (offset) => {
+    setLoading(true);
+    setTimeout(() => {
+      firebase
+        .database()
+        .ref("rooms")
+        .orderByKey()
+        .startAt(offset || "")
+        .limitToFirst(16)
+        .on(
+          "value",
+          (data) => {
+            const table = [];
+            data.forEach((e) => {
+              let value = e.exportVal();
+              value.chatRoom = e.key;
+              table.push(value);
+            });
+            if (table.length <= 1) {
+              setHasNextPage(false);
+            }
+            delete rooms[rooms.length - 1]; // remove last element cuz it"s first one in the new added list
+            setRooms(rooms.concat(table));
+            setLoading(false);
+          },
+          (err) => {
+            if (err) {
+              setError(err.message);
+            }
+          }
+        );
+    }, 500);
+  };
+
+  const setRandomRooms = () => {
+    for (let i = 0; i < 50; i++) {
+      firebase
+        .database()
+        .ref("rooms")
+        .child(Math.floor(Math.random(20) * 10000) + " ")
+        .set({
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+          username: Math.floor(Math.random(20) * 10000),
+          userId: Math.floor(Math.random(30) * 50000),
+        });
+    }
+  };
+  useEffect(() => {
+    getRooms();
+    setTimeout(() => {
+      setLoadingPage(false);
+    }, 1000);
+    //setRandomRooms();
+  }, []);
+
+  const infiniteRef = useInfiniteScroll({
+    loading,
+    hasNextPage,
+    onLoadMore: () => {
+      getRooms(rooms[rooms.length - 1].chatRoom);
+    },
+    scrollContainer: "window",
+  });
+  if (loadingPage) {
+    return <LoadingPage />;
+  }
+  return (
+    <Container className="mt-4" style={{ position: "relative" }}>
+      <Input type="text" placeholder="Search for users" />
+      {error && <Alert color="danger">{error}</Alert>}
+      <Row>
+        {rooms.map((user, index) => (
+          <Col key={user.chatRoom}>
+            <UserChatRoom
+              roomId={user.chatRoom}
+              userLogo={user.photo}
+              username={user.username}
+              lastMessage={
+                "Last seen " + new Date(user.updatedAt).toLocaleString()
+              }
+              status={user.status}
+            />
+          </Col>
+        ))}
+        {rooms.length > 0 && <div ref={infiniteRef}></div>}
+      </Row>
+      {!hasNextPage && (
+        <h4 className="text-center text-warning"> "No more users" </h4>
+      )}
+      {loading && (
+        <div
+          style={{
+            width: "100%",
+            padding: "20px",
+            marginBottom: "20px",
+            textAlign: "center",
+          }}
+        >
+          <Spinner
+            color="primary"
+            style={{
+              width: "100px",
+              height: "100px",
+              margin: "auto",
+            }}
+          />
+        </div>
+      )}
+    </Container>
+  );
+
+  /*const [users, setUsers] = useState([]);
   const [error, setError] = useState("");
   const [redirect, setRedirect] = useState({
     valid: false,
@@ -99,5 +218,5 @@ export default function Inbox() {
         </Row>
       </Container>
     </>
-  );
+  );*/
 }
