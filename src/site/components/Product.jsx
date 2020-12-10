adc93313bf31f40fd7037a49186f0cb0a5804177
Amor Ali <alibenamor30@gmail.com>
2020-12-10 16:15:03 +0100

    styling

--- src/site/components/Product.jsx
+++ src/site/components/Product.jsx
@@ -1,33 +1,79 @@
 import React from "react";
-
-export default function Product({ title, sale, img, imgAlt, ...rest }) {
+import { useHistory } from "react-router-dom";
+import "./Product.scss";
+export default function Product({
+  path,
+  title,
+  sale,
+  img,
+  imgAlt,
+  category,
+  price,
+  description,
+  productBgColor,
+  ...rest
+}) {
+  const history = useHistory();
   return (
-    <div className="product-card" {...rest}>
-      <h2 className="text-center text-wrap">{title || "Title"}</h2>
+    <div
+      className="product-card shadow-4 bg-materialgray"
+      {...rest}
+      onClick={(e) => {
+        if (
+          !e.target.className.includes("fas") &&
+          !e.target.className.includes("btn")
+        ) {
+          history.push(path);
+        }
+      }}
+    >
+      <div className="w-75 mx-auto">
+        <h2 className="text-center text-wrap pt-3 text-primary">
+          {title || "Title"}
+        </h2>
+        <p
+          className="h6 mt-3 text-left font-light text-wrap category text-secondary-2"
+          style={{ opacity: 0.7 }}
+        >
+          {category || "Category"}
+        </p>
+      </div>
       {sale && <span className="sales">{sale}</span>}
       <div className="body">
         <div className="left">
-          <div className="product-img">
-            <div className="like">
-              <button className="btn">
-                <i className="fas fa-heart fa-2x"></i>
-              </button>
+          <div
+            className="content"
+            style={{ backgroundColor: productBgColor || "unset" }}
+          >
+            <div className="product-img">
+              <div className="actions">
+                <div className="like">
+                  <button className="btn">
+                    <i className="fas fa-heart fa-2x"></i>
+                  </button>
+                </div>
+                <div className="share">
+                  <button className="btn">
+                    <i className="fas fa-share"></i>
+                  </button>
+                </div>
+              </div>
+              <img
+                src={
+                  img ||
+                  "https://images.unsplash.com/photo-1556912743-54b370e8385b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+                }
+                alt={imgAlt || "Product image"}
+              />
             </div>
-            <div className="share">
-              <button className="btn">
-                <i className="fas fa-share"></i>
-              </button>
-            </div>
-            <img
-              src={
-                img ||
-                "https://images.unsplash.com/photo-1556912743-54b370e8385b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-              }
-              alt={imgAlt || "Product image"}
-            />
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="content">
+            <p className="h3 price">{price || "Price"}</p>
+            <p className="h5 description">{description || "Description"}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/site/pages/admin/Products.js
+++ src/site/pages/admin/Products.js
@@ -1,33 +1,20 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
-import { Link } from "react-router-dom";
+import ProductCard from "../../components/Product";
 export default function Product() {
   return (
     <Container className="mt-3">
       <Row>
         <Col md>
           {" "}
-          <Link to="/admin/products/p1">Product1</Link>
+          <ProductCard
+            productBgColor="rgb(64, 128, 224)"
+            path="/admin/products/p1"
+          />
         </Col>
         <Col md>
           {" "}
-          <Link to="/admin/products/p2">Product2</Link>
-        </Col>
-        <Col md>
-          {" "}
-          <Link to="/admin/products/p3">Product3</Link>
-        </Col>
-        <Col md>
-          {" "}
-          <Link to="/admin/products/p4">Product4</Link>
-        </Col>
-        <Col md>
-          {" "}
-          <Link to="/admin/products/p5">Product5</Link>
-        </Col>
-        <Col md>
-          {" "}
-          <Link to="/admin/products/p6">Product6</Link>
+          <ProductCard path="/admin/products/p2" />
         </Col>
       </Row>
     </Container>
