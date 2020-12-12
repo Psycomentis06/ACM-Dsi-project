ed00b33d0eb27bd807a49f00f9a718806fb325dc
Amor Ali <alibenamor30@gmail.com>
2020-12-12 20:03:13 +0100

    add product box

---
+++ src/site/components/AddProduct.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function AddProduct() {
+  return (
+    <div>
+      <header>
+        <div className="ml-auto">
+          <button className="btn">
+            <i className="fas fa-close"></i>
+          </button>
+        </div>
+      </header>
+      <section></section>
+    </div>
+  );
+}
