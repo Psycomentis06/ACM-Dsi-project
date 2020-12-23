77a6538630d6b2001573884490f38df4a43da672
Amor Ali <alibenamor30@gmail.com>
2020-12-23 21:14:08 +0100

    delete read images only filter remain

--- src/site/pages/admin/UploadedImages.jsx
+++ src/site/pages/admin/UploadedImages.jsx
@@ -1,69 +1,111 @@
 // route for admin to let him manage uploaded images
 import React, { useEffect, useState } from "react";
-import { Modal, ModalBody, Alert, Spinner } from "reactstrap";
+import { Modal, ModalBody, Alert, Spinner, Input, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 import firebase from "../../../firebase.config";
 import "./UploadedImages.scss";
 export default function UploadedImages() {
+  const swal = withReactContent(Swal);
   const [images, setImages] = useState([]);
   const [error, setError] = useState("");
   const [imgPreviewModal, setImgPreviewModal] = useState(false);
   const [imgPreviewUrl, setImgPreviewUrl] = useState("");
   const [loading, setLoading] = useState(true);
   const openModal = (e, img) => {
-    if (!e.target.className.includes("btn")) {
+    if (!e.target.className.includes("btn") || e.target.nodeName === "A") {
       setImgPreviewModal(true);
       setImgPreviewUrl(img);
     }
   };
   const getUploads = (type) => {
+    let foldersToFetch = [];
+    if (type === "users") {
+      foldersToFetch = ["users"];
+    } else if (type === "products") {
+      foldersToFetch = ["products"];
+    } else {
+      foldersToFetch = ["users", "products"];
+    }
     firebase
       .storage()
       .ref("/")
       .listAll()
       .then((res) => {
         res.prefixes.forEach((folderRef) => {
-          folderRef
-            .listAll()
-            .then((res) => {
-              res.items.forEach((img) => {
-                img
-                  .getMetadata()
-                  .then((res) => {})
-                  .catch((err) => {
-                    setError(err.message);
-                  });
-              });
-            })
-            .catch((err) => {
-              setError(err.message);
-            });
+          if (foldersToFetch.includes(folderRef.name)) {
+            folderRef
+              .listAll()
+              .then((res) => {
+                res.items.forEach((img) => {
+                  img
+                    .getMetadata()
+                    .then((res) => {
+                      let imageData = {
+                        timeCreated: res.timeCreated,
+                        user: res.customMetadata?.username || "Not mentioned",
+                        userId: res.customMetadata?.userId || "",
+                        category: folderRef.name,
+                        size: res.size,
+                        imagePath: res.fullPath,
+                      };
+
+                      img.getDownloadURL().then((res) => {
+                        imageData.imageUrl = res;
+                        setImages((prevState) => [...prevState, imageData]);
+                      });
+                    })
+                    .catch((err) => {
+                      setError(err.message);
+                    });
+                });
+              })
+              .catch((err) => {
+                setError(err.message);
+              })
+              .finally(() => setLoading(false));
+          }
         });
       });
-    /*.then((res) => {
-          
-        res.items.forEach((itemRef) => {
-          itemRef
-            .getDownloadURL()
-            .then((res) => setImages((prevState) => [...prevState, res]))
-            .catch((err) => setError(err.message));
-        });
-      })
-      .catch((err) => {
-        setError(err.message);
-      })
-      .finally(() => setLoading(false));*/
   };
   useEffect(() => {
-    getUploads("");
+    getUploads();
   }, []);
   return (
     <div className="uploads-page">
+      <Row className="my-3 w-75 mx-auto">
+        <Col>
+          <Input placeholder="Search for image by user " />
+        </Col>
+        <Col className="text-center">
+          <Row>
+            <Col>
+              <label> All </label>
+              <label>Users</label>
+              <label>Products</label>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <label>
+                <Input type="radio" name="category" />
+              </label>
+              <label>
+                <Input type="radio" name="category" />
+              </label>
+              <label>
+                <Input type="radio" name="category" />
+              </label>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
       {error.length > 0 && <Alert color="danger"> {error} </Alert>}
       <Modal
         isOpen={imgPreviewModal}
         toggle={() => setImgPreviewModal(!imgPreviewModal)}
         size="lg"
-        centred={true}
       >
         <ModalBody>
           <img src={imgPreviewUrl} alt="Preview" className="img-fluid" />
@@ -75,28 +117,78 @@ export default function UploadedImages() {
       <div className="photo-grid">
         {images.map((img, index) => (
           <div
+            key={index}
             className={
               " shadow-1 card  " +
               (index % 8 === 0 ? " card-tall card-wide " : "") +
               (index % 3 === 0 ? " card-tall " : "") +
               (index % 7 === 0 ? " card-wide " : "")
             }
-            onClick={(e) => openModal(e, img)}
+            onClick={(e) => openModal(e, img.imageUrl)}
           >
             <div className="description">
               <ul>
-                <li className="items">Uploaded By: </li>
-                <li className="items">Upload date: </li>
-                <li className="items">Category: </li>
+                <li className="items">
+                  Uploaded By:{" "}
+                  <Link
+                    to={img.userId}
+                    style={{
+                      pointerEvents: img.userId.length === 0 ? "none" : "auto",
+                    }}
+                  >
+                    {" "}
+                    {img.user}{" "}
+                  </Link>
+                </li>
+                <li className="items">
+                  Upload date:{" "}
+                  {new Date(img.timeCreated).toLocaleDateString() +
+                    " : " +
+                    new Date(img.timeCreated).toLocaleTimeString()}{" "}
+                </li>
                 <li className="items">
                   {" "}
-                  <button className="btn btn-danger w-50">
+                  Image Size: {Math.round(img.size / 1024)} KB
+                </li>
+                <li className="items">Category: {img.category} </li>
+                <li className="items">
+                  {" "}
+                  <button
+                    className="btn btn-danger w-50"
+                    onClick={() => {
+                      swal.fire({
+                        title: "Delete Image",
+                        text:
+                          "You are going to delete this image from database. User will not find it next time or your product may displayed without image are you sure about that ?",
+                        icon: "question",
+                        showConfirmButton: true,
+                        confirmButtonText: "Delete",
+                        confirmButtonColor: "danger",
+                        showCancelButton: true,
+                        cancelButtonText: "Go Back",
+                        showLoaderOnConfirm: true,
+                        preConfirm: () => {
+                          firebase
+                            .storage()
+                            .ref(img.imagePath)
+                            .delete()
+                            .then((res) => {
+                              swal.fire("Success", "Image deleted", "success");
+                              getUploads("all");
+                            })
+                            .catch((err) =>
+                              swal.fire("Error", err.message, "error")
+                            );
+                        },
+                      });
+                    }}
+                  >
                     <i className="fas fa-trash"></i>
                   </button>{" "}
                 </li>
               </ul>
             </div>
-            <img src={img} alt="uploaded avatar or product preview" />
+            <img src={img.imageUrl} alt="uploaded avatar or product preview" />
           </div>
         ))}
         {loading && (
