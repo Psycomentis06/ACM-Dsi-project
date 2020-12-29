5d2b6b2f9e020700921e71f1c0d9e9c816091ebd
Amor Ali <alibenamor30@gmail.com>
2020-12-29 23:18:42 +0100

    Stats from api

--- src/site/pages/admin/Dashboard.js
+++ src/site/pages/admin/Dashboard.js
@@ -1,9 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Container, Row, Col } from "reactstrap";
 import { Line } from "react-chartjs-2";
+import { Redirect } from "react-router-dom";
+import Axios from "axios";
+import errorHandler from "../../functions/errorHandler";
 import StatCard from "../../components/StatCard";
 export default function Dashboard() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [chart, setChart] = useState({ charOne: [], chartTwo: [] });
+  const [stats, setStats] = useState({
+    likes: 0,
+    usersMonthly: 0,
+    usersDaily: 0,
+    orders: 0,
+  });
+  const getHistoryByMonth = () => {
+    Axios.get(process.env.REACT_APP_API_URL + "/history/month", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    })
+      .then((response) => {
+        if (response.data.valid === true) {
+          let monthlyUsers = 0;
+          response.data.data.map((item) => {
+            if (
+              new Date(item.day).toDateString() === new Date().toDateString()
+            ) {
+              // today
+              setStats((prevState) => ({
+                ...prevState,
+                likes: item.likedProducts,
+                orders: item.orders,
+                usersDaily: item.loggedUsers,
+              }));
+            }
+            monthlyUsers += item.loggedUsers;
+          });
+          setStats((prevState) => ({
+            ...prevState,
+            usersMonthly: monthlyUsers,
+          }));
+        } else {
+          setError("Unhandled response");
+        }
+      })
+      .catch((err) => {
+        const error = errorHandler(err);
+        if (error.type === "error") {
+          setError(error.message);
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: error.path,
+                state: { path: "/admin", message: error.message },
+              }}
+            />
+          );
+        }
+      });
+  };
+  useEffect(() => getHistoryByMonth(), []);
   return (
     <>
       <Container fluid={true}>
@@ -23,7 +83,7 @@ export default function Dashboard() {
                     visibility
                   </span>
                 </div>
-                <h6 className="mt-1">500</h6>
+                <h6 className="mt-1"> {stats.usersDaily} </h6>
                 <h3>Daily Visits</h3>
               </StatCard>
             </Col>
@@ -40,7 +100,7 @@ export default function Dashboard() {
                     favorite
                   </span>
                 </div>
-                <h6 className="mt-1">4.5k</h6>
+                <h6 className="mt-1"> {stats.likes} </h6>
                 <h3>Likes</h3>
               </StatCard>
             </Col>
@@ -57,7 +117,7 @@ export default function Dashboard() {
                     group
                   </span>
                 </div>
-                <h6 className="mt-1">30.6K</h6>
+                <h6 className="mt-1"> {stats.usersMonthly} </h6>
                 <h3>Global Visits</h3>
               </StatCard>
             </Col>
@@ -74,7 +134,7 @@ export default function Dashboard() {
                     shopping_cart
                   </span>
                 </div>
-                <h6 className="mt-1">650</h6>
+                <h6 className="mt-1"> {stats.orders} </h6>
                 <h3>Daily orders</h3>
               </StatCard>
             </Col>
